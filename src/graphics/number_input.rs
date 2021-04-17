0ec12dcbb7651dfdb729b9f1f92144bdd0e2461b
leang27 <52003343+leang27@users.noreply.github.com>
2021-04-17 15:44:52 +0700

    Formatting

--- src/graphics/number_input.rs
+++ src/graphics/number_input.rs
@@ -17,109 +17,109 @@ pub type NumberInput<'a, T, Message, Backend> = number_input::NumberInput<'a, T,
 
 impl<B> number_input::Renderer for Renderer<B>
 where
-   B: Backend + backend::Text,
+    B: Backend + backend::Text,
 {
-   type Style = Box<dyn StyleSheet>;
+    type Style = Box<dyn StyleSheet>;
 
-   const DEFAULT_PADDING: u16 = 5;
+    const DEFAULT_PADDING: u16 = 5;
 
-   fn draw(
-      &mut self,
-      cursor_position: Point,
-      state: &ModifierState,
-      inc_bounds: Rectangle,
-      dec_bounds: Rectangle,
-      is_mouse_over: bool,
-      is_decrease_disabled: bool,
-      is_increase_disabled: bool,
-      (content, _): Self::Output,
-      style: &<Self as number_input::Renderer>::Style,
-      font: Self::Font,
-   ) -> Self::Output {
-      let mouse_over_decrease = dec_bounds.contains(cursor_position);
-      let mouse_over_increase = inc_bounds.contains(cursor_position);
+    fn draw(
+        &mut self,
+        cursor_position: Point,
+        state: &ModifierState,
+        inc_bounds: Rectangle,
+        dec_bounds: Rectangle,
+        is_mouse_over: bool,
+        is_decrease_disabled: bool,
+        is_increase_disabled: bool,
+        (content, _): Self::Output,
+        style: &<Self as number_input::Renderer>::Style,
+        font: Self::Font,
+    ) -> Self::Output {
+        let mouse_over_decrease = dec_bounds.contains(cursor_position);
+        let mouse_over_increase = inc_bounds.contains(cursor_position);
 
-      let decrease_btn_style = if is_decrease_disabled {
-         style.disabled()
-      } else if state.decrease_pressed {
-         style.pressed()
-      } else {
-         style.active()
-      };
+        let decrease_btn_style = if is_decrease_disabled {
+            style.disabled()
+        } else if state.decrease_pressed {
+            style.pressed()
+        } else {
+            style.active()
+        };
 
-      let increase_btn_style = if is_increase_disabled {
-         style.disabled()
-      } else if state.increase_pressed {
-         style.pressed()
-      } else {
-         style.active()
-      };
+        let increase_btn_style = if is_increase_disabled {
+            style.disabled()
+        } else if state.increase_pressed {
+            style.pressed()
+        } else {
+            style.active()
+        };
 
-      // decrease button section
-      let decrease_button_rect = Primitive::Quad {
-         bounds: dec_bounds,
-         background: decrease_btn_style
-            .button_background
-            .unwrap_or(Background::Color(Color::TRANSPARENT)),
-         border_radius: 3.0,
-         border_width: 0.,
-         border_color: Color::TRANSPARENT,
-      };
-      let decrease_text = Primitive::Text {
-         content: String::from("▼"),
-         bounds: Rectangle {
-            x: dec_bounds.center_x(),
-            y: dec_bounds.center_y(),
-            ..dec_bounds
-         },
-         font,
-         size: dec_bounds.height * 0.9,
-         color: decrease_btn_style.icon_color,
-         horizontal_alignment: HorizontalAlignment::Center,
-         vertical_alignment: VerticalAlignment::Center,
-      };
-      let decrease_btn = Primitive::Group {
-         primitives: vec![decrease_button_rect, decrease_text],
-      };
+        // decrease button section
+        let decrease_button_rect = Primitive::Quad {
+            bounds: dec_bounds,
+            background: decrease_btn_style
+                .button_background
+                .unwrap_or(Background::Color(Color::TRANSPARENT)),
+            border_radius: 3.0,
+            border_width: 0.,
+            border_color: Color::TRANSPARENT,
+        };
+        let decrease_text = Primitive::Text {
+            content: String::from("\u{25bc}"),
+            bounds: Rectangle {
+                x: dec_bounds.center_x(),
+                y: dec_bounds.center_y(),
+                ..dec_bounds
+            },
+            font,
+            size: dec_bounds.height * 0.9,
+            color: decrease_btn_style.icon_color,
+            horizontal_alignment: HorizontalAlignment::Center,
+            vertical_alignment: VerticalAlignment::Center,
+        };
+        let decrease_btn = Primitive::Group {
+            primitives: vec![decrease_button_rect, decrease_text],
+        };
 
-      // increase button section
-      let increase_button_rect = Primitive::Quad {
-         bounds: inc_bounds,
-         background: increase_btn_style
-            .button_background
-            .unwrap_or(Background::Color(Color::TRANSPARENT)),
-         border_radius: 3.0,
-         border_width: 0.,
-         border_color: Color::TRANSPARENT,
-      };
-      let increase_text = Primitive::Text {
-         content: String::from("▲"),
-         bounds: Rectangle {
-            x: inc_bounds.center_x(),
-            y: inc_bounds.center_y(),
-            ..inc_bounds
-         },
-         font,
-         size: inc_bounds.height * 0.9,
-         color: increase_btn_style.icon_color,
-         horizontal_alignment: HorizontalAlignment::Center,
-         vertical_alignment: VerticalAlignment::Center,
-      };
-      let increase_btn = Primitive::Group {
-         primitives: vec![increase_button_rect, increase_text],
-      };
+        // increase button section
+        let increase_button_rect = Primitive::Quad {
+            bounds: inc_bounds,
+            background: increase_btn_style
+                .button_background
+                .unwrap_or(Background::Color(Color::TRANSPARENT)),
+            border_radius: 3.0,
+            border_width: 0.,
+            border_color: Color::TRANSPARENT,
+        };
+        let increase_text = Primitive::Text {
+            content: String::from("\u{25b2}"),
+            bounds: Rectangle {
+                x: inc_bounds.center_x(),
+                y: inc_bounds.center_y(),
+                ..inc_bounds
+            },
+            font,
+            size: inc_bounds.height * 0.9,
+            color: increase_btn_style.icon_color,
+            horizontal_alignment: HorizontalAlignment::Center,
+            vertical_alignment: VerticalAlignment::Center,
+        };
+        let increase_btn = Primitive::Group {
+            primitives: vec![increase_button_rect, increase_text],
+        };
 
-      (
-         Primitive::Group {
-            primitives: vec![content, decrease_btn, increase_btn],
-         },
-         if (mouse_over_decrease && !is_decrease_disabled) || (mouse_over_increase && !is_increase_disabled) {
-            mouse::Interaction::Pointer
-         } else if is_mouse_over {
-            mouse::Interaction::Text
-         } else {
-            mouse::Interaction::default()
-         },
-      )
-   }
+        (
+            Primitive::Group {
+                primitives: vec![content, decrease_btn, increase_btn],
+            },
+            if (mouse_over_decrease && !is_decrease_disabled) || (mouse_over_increase && !is_increase_disabled) {
+                mouse::Interaction::Pointer
+            } else if is_mouse_over {
+                mouse::Interaction::Text
+            } else {
+                mouse::Interaction::default()
+            },
+        )
+    }
 }
--- src/style/number_input.rs
+++ src/style/number_input.rs
@@ -1,6 +1,6 @@
 //! Display fields that can only be filled with numeric type.
 //!
-//! *This API requires the following crate features to be activated: number_input
+//! *This API requires the following crate features to be activated: `number_input`*
 #[cfg(not(target_arch = "wasm32"))]
 use iced_native::{Background, Color};
 #[cfg(target_arch = "wasm32")]
@@ -9,48 +9,48 @@ use iced_web::{Background, Color};
 /// The appearance of a [`NumberInput`](crate::native::number_input::NumberInput).
 #[derive(Clone, Copy, Debug)]
 pub struct Style {
-   /// The background of the [`NumberInput`](crate::native::number_input::NumberInput).
-   pub button_background: Option<Background>,
-   /// The Color of the arrows of [`NumberInput`](crate::native::number_input::NumberInput).
-   pub icon_color: Color,
+    /// The background of the [`NumberInput`](crate::native::number_input::NumberInput).
+    pub button_background: Option<Background>,
+    /// The Color of the arrows of [`NumberInput`](crate::native::number_input::NumberInput).
+    pub icon_color: Color,
 }
 
 impl std::default::Default for Style {
-   fn default() -> Self {
-      Self {
-         button_background: None,
-         icon_color: Color::BLACK,
-      }
-   }
+    fn default() -> Self {
+        Self {
+            button_background: None,
+            icon_color: Color::BLACK,
+        }
+    }
 }
 
 /// The appearance of a [`NumberInput`](crate::native::number_input::NumberInput).
 pub trait StyleSheet {
-   /// The normal appearance of a [`NumberInput`](crate::native::number_input::NumberInput).
-   fn active(&self) -> Style;
+    /// The normal appearance of a [`NumberInput`](crate::native::number_input::NumberInput).
+    fn active(&self) -> Style;
 
-   /// The appearance when the [`NumberInput`](crate::native::number_input::NumberInput) is pressed.
-   fn pressed(&self) -> Style {
-      self.active()
-   }
+    /// The appearance when the [`NumberInput`](crate::native::number_input::NumberInput) is pressed.
+    fn pressed(&self) -> Style {
+        self.active()
+    }
 
-   /// The appearance when the [`NumberInput`](crate::native::number_input::NumberInput) is disabled.
-   fn disabled(&self) -> Style {
-      let active = self.active();
-      Style {
-         button_background: active.button_background.map(|bg| match bg {
-            Background::Color(color) => Background::Color(Color {
-               a: color.a * 0.5,
-               ..color
+    /// The appearance when the [`NumberInput`](crate::native::number_input::NumberInput) is disabled.
+    fn disabled(&self) -> Style {
+        let active = self.active();
+        Style {
+            button_background: active.button_background.map(|bg| match bg {
+                Background::Color(color) => Background::Color(Color {
+                a: color.a * 0.5,
+                ..color
+                }),
             }),
-         }),
-         icon_color: Color {
-            a: active.icon_color.a * 0.5,
-            ..active.icon_color
-         },
-         ..active
-      }
-   }
+            icon_color: Color {
+                a: active.icon_color.a * 0.5,
+                ..active.icon_color
+            },
+            ..active
+        }
+    }
 }
 
 /// The default appearance of the [`NumberInput`](crate::native::number_input::NumberInput).
@@ -58,22 +58,24 @@ pub trait StyleSheet {
 struct Default;
 
 impl StyleSheet for Default {
-   fn active(&self) -> Style {
-      Style::default()
-   }
+    fn active(&self) -> Style {
+        Style::default()
+    }
 }
 
+#[allow(clippy::use_self)]
 impl std::default::Default for Box<dyn StyleSheet> {
-   fn default() -> Self {
-      Box::new(Default)
-   }
+    fn default() -> Self {
+        Box::new(Default)
+    }
 }
 
+#[allow(clippy::use_self)]
 impl<T> From<T> for Box<dyn StyleSheet>
 where
-   T: 'static + StyleSheet,
+    T: 'static + StyleSheet,
 {
-   fn from(style: T) -> Self {
-      Box::new(style)
-   }
+    fn from(style: T) -> Self {
+        Box::new(style)
+    }
 }
