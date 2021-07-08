86b6a0a2ff07ba5e42c8fb4d8a463d2a0e5e02cf
Andrew Wheeler <genusistimelord@gmail.com>
2021-07-08 08:31:19 -0400

    Adding Selection List Fixes to Beta

--- src/style/selection_list.rs
+++ src/style/selection_list.rs
@@ -1,34 +1,62 @@
-//! Selection List Redirects a Menu Style.
-use iced_style::menu;
+//! Selection List
+//!
+//! *This API requires the following crate features to be activated: `selection_list`*
+use iced_native::{Background, Color, Length};
+
+/// The appearance of a menu.
+#[derive(Debug, Clone, Copy)]
+pub struct Style {
+    /// The List Label Text Color
+    pub text_color: Color,
+    /// The background
+    pub background: Background,
+    /// The container Border width
+    pub border_width: f32,
+    /// The container Border color
+    pub border_color: Color,
+    /// The List Label Text Select Color
+    pub selected_text_color: Color,
+    /// The List Label Text Select Background Color
+    pub selected_background: Background,
+    /// The Containers Width
+    pub width: Length,
+    /// The Containers height
+    pub height: Length,
+    /// The padding Width
+    pub padding: u16,
+    /// The Text Size
+    pub text_size: u16,
+}
+
+impl std::default::Default for Style {
+    fn default() -> Self {
+        Self {
+            text_color: Color::BLACK,
+            background: Background::Color([0.87, 0.87, 0.87].into()),
+            border_width: 1.0,
+            border_color: [0.7, 0.7, 0.7].into(),
+            selected_text_color: Color::WHITE,
+            selected_background: Background::Color([0.4, 0.4, 1.0].into()),
+            width: Length::Fill,
+            height: Length::Fill,
+            padding: 5,
+            text_size: 12,
+        }
+    }
+}
 
 /// A set of rules that dictate the style of a container.
 pub trait StyleSheet {
-    /// The normal appearance of a [`ListMenu`]
-    fn menu(&self) -> menu::Style;
+    /// Produces the style of a container.
+    fn style() -> Style;
 }
 
-/// Default holder
+/// default Style holder.
 #[derive(Clone, Copy, Debug)]
-struct Default;
+pub struct Default;
 
 impl StyleSheet for Default {
-    fn menu(&self) -> menu::Style {
-        menu::Style::default()
-    }
-}
-#[allow(clippy::use_self)]
-impl std::default::Default for Box<dyn StyleSheet> {
-    fn default() -> Self {
-        Box::new(Default)
-    }
-}
-
-#[allow(clippy::use_self)]
-impl<T> From<T> for Box<dyn StyleSheet>
-where
-    T: 'static + StyleSheet,
-{
-    fn from(style: T) -> Self {
-        Box::new(style)
+    fn style() -> Style {
+        Style::default()
     }
 }
