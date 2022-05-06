d1deac7f199dfc85d698fff3382111ebcaae3901
Kaiden42 <gitlab@tinysn.com>
2022-05-06 21:42:58 +0200

    Make date_picker pure

---
+++ examples/pure/date_picker/src/main.rs
@@ -0,0 +1,80 @@
+use iced::{
+    pure::{
+        widget::{Button, Container, Row, Text},
+        Element, Sandbox,
+    },
+    Alignment, Length, Settings,
+};
+use iced_aw::{date_picker::Date, pure::DatePicker};
+
+fn main() -> iced::Result {
+    DatePickerExample::run(Settings::default())
+}
+
+#[derive(Clone, Debug)]
+#[allow(clippy::enum_variant_names)]
+enum Message {
+    ChooseDate,
+    SubmitDate(Date),
+    CancelDate,
+}
+
+struct DatePickerExample {
+    date: Date,
+    show_picker: bool,
+}
+
+impl Sandbox for DatePickerExample {
+    type Message = Message;
+
+    fn new() -> Self {
+        DatePickerExample {
+            date: Date::default(),
+            show_picker: false,
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("DatePicker example")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::ChooseDate => {
+                //self.state.reset();
+                self.show_picker = true;
+            }
+            Message::SubmitDate(date) => {
+                self.date = date;
+                self.show_picker = false;
+            }
+            Message::CancelDate => {
+                self.show_picker = false;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        let but = Button::new(Text::new("Set Date")).on_press(Message::ChooseDate);
+
+        let datepicker = DatePicker::new(
+            self.show_picker,
+            but,
+            Message::CancelDate,
+            Message::SubmitDate,
+        );
+
+        let row = Row::new()
+            .align_items(Alignment::Center)
+            .spacing(10)
+            .push(datepicker)
+            .push(Text::new(format!("Date: {}", self.date,)));
+
+        Container::new(row)
+            .center_x()
+            .center_y()
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
+    }
+}
--- src/pure/mod.rs
+++ src/pure/mod.rs
@@ -14,3 +14,8 @@ pub use card::Card;
 pub mod color_picker;
 #[cfg(feature = "color_picker")]
 pub use color_picker::ColorPicker;
+
+#[cfg(feature = "date_picker")]
+pub mod date_picker;
+#[cfg(feature = "date_picker")]
+pub use date_picker::DatePicker;
