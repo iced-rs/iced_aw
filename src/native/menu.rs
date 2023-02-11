420b391ae8526dce989a2e8f9428ad0765d7178b
Latidoremi <latidoremi.001@gmail.com>
2023-02-11 23:19:14 +0800

    separate menu.rs

--- src/native/menu.rs
+++ src/native/menu.rs
@@ -4,8 +4,10 @@
 
 mod menu_tree;
 mod menu_bar;
+mod menu;
 mod flex;
 
 pub use menu_tree::MenuTree;
-pub use menu_bar::{MenuBar, PathHighlight};
+pub use menu_bar::MenuBar;
+pub use menu::PathHighlight;
 pub use crate::style::menu_bar::{Appearance, StyleSheet};
